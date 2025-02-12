4bba97fa016c18f5e045c1ac77a1fde1c15b9712
Nelson Gomez <n00gomez00@gmail.com>
2025-02-11 21:02:20 -0600

    FileSystem - Datasource

---
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,36 @@
+import { LogDataSource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogServerityLevel } from "../../domain/entities/log.entity";
+import * as fs from 'fs';
+
+
+export class FileSystemDataSource implements LogDataSource {
+  private readonly logPath = "logs/";
+  private readonly allLogsPath = "logs/logs-low.log";
+  private readonly mediumLogsPath = "logs/logs-medium.log";
+  private readonly highLogsPath = "logs/logs-high.log";
+
+  constructor() {
+    this.createLogsFiles();
+  }
+
+  private createLogsFiles = () => {
+    if (!fs.existsSync(this.logPath)) {
+      fs.mkdirSync(this.logPath);
+    }
+
+    [this.allLogsPath, this.mediumLogsPath, this.highLogsPath].forEach(
+      (path) => {
+        if (!fs.existsSync(path)) return;
+        fs.writeFileSync(path, "");
+      }
+    );
+  };
+
+  saveLog(log: LogEntity): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  getLogs(severityLevel: LogServerityLevel): Promise<LogEntity[]> {
+    throw new Error("Method not implemented.");
+  }
+}
