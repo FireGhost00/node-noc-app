2ed8ba059f738e9af38d8fa79f17e700902a4cc9
Nelson Gomez <n00gomez00@gmail.com>
2025-02-11 21:37:56 -0600

    Filesystem - GetLogs

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -21,5 +21,20 @@ export class LogEntity {
         this.createdAt = new Date();
     }
 
+    /**
+     * Creates an instance of LogEntity from a JSON string.
+     * 
+     * @param json - The JSON string to parse.
+     * @returns A new instance of LogEntity.
+     * @throws Will throw an error if the JSON string cannot be parsed.
+     */
+    static fromJson = (json: string): LogEntity =>{
+        
+        const { level, message, createdAt } = JSON.parse(json);
+        const log = new LogEntity(level, message);
+        log.createdAt = new Date(createdAt);
+        return log;
+    }
+
 
 }
--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -25,11 +25,58 @@ export class FileSystemDataSource implements LogDataSource {
     );
   };
 
-  saveLog(log: LogEntity): Promise<void> {
-    throw new Error("Method not implemented.");
+/**
+ * Saves a log entry to the appropriate log files based on its severity level.
+ *
+ * @param newLog - The log entry to be saved.
+ * @returns A promise that resolves when the log entry has been saved.
+ *
+ * The log entry is saved to a general log file. Additionally, if the log entry's
+ * severity level is medium or high, it is also saved to a specific log file for
+ * that severity level.
+ *
+ * @remarks
+ * - Logs with a severity level of `low` are only saved to the general log file.
+ * - Logs with a severity level of `medium` are saved to both the general log file
+ *   and the medium severity log file.
+ * - Logs with a severity level of `high` are saved to both the general log file
+ *   and the high severity log file.
+ */
+  async saveLog(newLog: LogEntity): Promise<void> {
+    const logAsjson = `${JSON.stringify(newLog)}\n`;
+
+    fs.appendFileSync(this.allLogsPath, logAsjson);
+
+    if (newLog.level === LogServerityLevel.low) return;
+
+    if (newLog.level === LogServerityLevel.medium) {
+      fs.appendFileSync(this.mediumLogsPath, logAsjson);
+    } else {
+      fs.appendFileSync(this.highLogsPath, logAsjson);
+    }
   }
 
-  getLogs(severityLevel: LogServerityLevel): Promise<LogEntity[]> {
-    throw new Error("Method not implemented.");
+    private  getLogsFromFile = (path: string): LogEntity[] => {
+        const content = fs.readFileSync(path, "utf-8");
+        const logs = content.split("\n").map(LogEntity.fromJson);
+        return logs;
+    
+    }
+  
+  async getLogs(severityLevel: LogServerityLevel): Promise<LogEntity[]> {
+    
+    switch (severityLevel) {
+        case LogServerityLevel.low:
+            return this.getLogsFromFile(this.allLogsPath);
+        case LogServerityLevel.medium:
+            return this.getLogsFromFile(this.mediumLogsPath);
+        case LogServerityLevel.high:
+            return this.getLogsFromFile(this.highLogsPath);
+        default:
+            throw new Error(`${severityLevel} not implemented`);
+    }
+
+
+
   }
 }
