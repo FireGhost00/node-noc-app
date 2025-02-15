380dd485cda7c1f2bfbef7c0006fa280e6fdb3b6
Nelson Gomez <n00gomez00@gmail.com>
2025-02-14 22:29:33 -0600

    Refactor PostgresLogDataSource to use SeverityLevel enum for log levels

--- src/infrastructure/datasources/postgres-log.datasource.ts
+++ src/infrastructure/datasources/postgres-log.datasource.ts
@@ -1,32 +1,41 @@
-import { $Enums, PrismaClient } from "@prisma/client";
+import { $Enums, PrismaClient, SeverityLevel } from "@prisma/client";
 import { LogDataSource } from "../../domain/datasources/log.datasource";
 import { LogEntity, LogServerityLevel } from "../../domain/entities/log.entity";
 
 const prismaClient = new PrismaClient();
+
+const serverityEnum = {
+    low: SeverityLevel.LOW,
+    medium: SeverityLevel.MEDIUM,
+    high: SeverityLevel.HIGH,
+}
+
 
 export class PostgresLogDataSource implements LogDataSource {
 
 
 
 
+    
   async  saveLog(log: LogEntity): Promise<void> {
+
+        const level = serverityEnum[log.level as keyof typeof serverityEnum];
         const newLog = await prismaClient.logModel.create({
             data: {
-                level: log.level.toUpperCase() as any,
-                message: log.message,
-                origin: log.origin,
+                ...log,
+                level: level,
             }
         });
     
+    
     }
     async getLogs(severityLevel: LogServerityLevel): Promise<LogEntity[]> {
-        const logs = await prismaClient.logModel.findMany({
-            where: {
-                level: severityLevel.toUpperCase() as any,
-            }
+        const level = serverityEnum[severityLevel as keyof typeof serverityEnum];
+        const dbLogs = await prismaClient.logModel.findMany({
+            where: {level}
         });
 
-        return logs.map(LogEntity.fromObject);
+        return dbLogs.map(LogEntity.fromObject);
     }
 
 }
